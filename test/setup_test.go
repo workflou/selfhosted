ba6f22e32811880c21b2c82803f88f58ece529a2
Krzysztof <krzysztof@workflou.com>
2025-05-24 21:03:05 +0200

    restructure tests

--- test/setup_test.go
+++ test/setup_test.go
@@ -5,23 +5,25 @@ import (
 	"testing"
 )
 
-func TestSetupRedirect(t *testing.T) {
-	tc := NewTestCase(t)
-	defer tc.Close()
+func TestSetup(t *testing.T) {
+	t.Run("redirect to setup when no admin account exists", func(t *testing.T) {
+		tc := NewTestCase(t)
+		defer tc.Close()
 
-	tc.Get("/")
-	tc.AssertRedirect(http.StatusSeeOther, "/setup")
-}
+		tc.Get("/")
+		tc.AssertRedirect(http.StatusSeeOther, "/setup")
+	})
 
-func TestSetupForm(t *testing.T) {
-	tc := NewTestCase(t)
-	defer tc.Close()
+	t.Run("setup form is displayed", func(t *testing.T) {
+		tc := NewTestCase(t)
+		defer tc.Close()
 
-	tc.Get("/setup")
+		tc.Get("/setup")
 
-	tc.AssertElementVisible("form[hx-post='/setup']")
-	tc.AssertElementVisible("input[name='name']")
-	tc.AssertElementVisible("input[name='email']")
-	tc.AssertElementVisible("input[name='password']")
-	tc.AssertElementVisible("button[type='submit']")
+		tc.AssertElementVisible("form[hx-post='/setup']")
+		tc.AssertElementVisible("input[name='name']")
+		tc.AssertElementVisible("input[name='email']")
+		tc.AssertElementVisible("input[name='password']")
+		tc.AssertElementVisible("button[type='submit']")
+	})
 }
