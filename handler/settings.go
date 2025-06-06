16a566760f1eb2144a1393b72349d5d62c0f1bbd
Krzysztof <krzysztof@engineer.com>
2025-06-06 11:46:21 +0200

    remove old avatars

--- handler/settings.go
+++ handler/settings.go
@@ -78,49 +78,42 @@ func SettingsAvatarForm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err == nil {
-		slog.Info("User avatar updated", "user_id", user.ID, "avatar_path", path)
-		err = store.New(database.DB).UpdateUserAvatar(r.Context(), store.UpdateUserAvatarParams{
-			ID: user.ID,
-			Avatar: sql.NullString{
-				String: "/" + path,
-				Valid:  true,
-			},
-		})
+	if err != nil {
+		slog.Error("File upload error", "error", err)
+		w.Header().Set("HX-Reswap", "none")
+		w.WriteHeader(http.StatusInternalServerError)
+		toast.Error("Upload failed", err.Error()).Send(r.Context(), w)
+		return
+	}
+
+	if user.Avatar.Valid {
+		err = os.Remove(filepath.Join(".", user.Avatar.String))
 		if err != nil {
-			slog.Error("Failed to update user avatar", "error", err)
-			w.Header().Set("HX-Reswap", "none")
-			w.WriteHeader(http.StatusInternalServerError)
-			toast.Error("Update failed", "An error occurred while updating your profile. Please try again later.").Send(r.Context(), w)
-			return
+			slog.Error("Failed to remove old avatar", "error", err)
 		}
-		user.Avatar = sql.NullString{
+	}
+
+	err = store.New(database.DB).UpdateUserAvatar(r.Context(), store.UpdateUserAvatarParams{
+		ID: user.ID,
+		Avatar: sql.NullString{
 			String: "/" + path,
 			Valid:  true,
-		}
-		html.UserAvatar(user.Avatar.String, user.Name).Render(r.Context(), w)
-		toast.Success("Avatar updated", "Your avatar has been successfully updated.").Send(r.Context(), w)
+		},
+	})
+	if err != nil {
+		slog.Error("Failed to update user avatar", "error", err)
+		w.Header().Set("HX-Reswap", "none")
+		w.WriteHeader(http.StatusInternalServerError)
+		toast.Error("Update failed", "An error occurred while updating your profile. Please try again later.").Send(r.Context(), w)
 		return
 	}
-
-	if name := r.FormValue("name"); name != "" {
-		err := store.New(database.DB).UpdateUserName(r.Context(), store.UpdateUserNameParams{
-			ID:   user.ID,
-			Name: name,
-		})
-		if err != nil {
-			w.Header().Set("HX-Reswap", "none")
-			w.WriteHeader(http.StatusInternalServerError)
-			toast.Error("Update failed", "An error occurred while updating your profile. Please try again later.").Send(r.Context(), w)
-			return
-		}
-		user.Name = name
-		html.UserName(user.Name).Render(r.Context(), w)
-		toast.Success("Name updated", "Your name has been successfully updated.").Send(r.Context(), w)
-		return
+	user.Avatar = sql.NullString{
+		String: "/" + path,
+		Valid:  true,
 	}
-
-	w.Header().Set("HX-Reswap", "none")
+	html.UserAvatar(user.Avatar.String, user.Name).Render(r.Context(), w)
+	toast.Success("Avatar updated", "Your avatar has been successfully updated.").Send(r.Context(), w)
+	return
 }
 
 func uploadFile(r *http.Request, fieldName string, uploadsPath string) (string, error) {
